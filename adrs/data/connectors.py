e7d00b5325defafd20b4526e263c2add8ebabc95
Jian Wen <wongjianwen0520@gmail.com>
2026-05-13 15:02:21 +0800

    feat: added publish and subcribe to db

--- adrs/data/connectors.py
+++ adrs/data/connectors.py
@@ -1,10 +1,17 @@
-from typing import Protocol, AsyncIterator
+from nats.aio.msg import Msg
+from typing import Protocol, AsyncIterator, Callable, Awaitable
 
 from adrs.types import Topic, Message
 
 
 class Database(Protocol):
-    async def insert(self, subject: str, payload: bytes) -> None: ...
+    async def subscribe(
+        self, subject: str, callback: Callable[[Msg], Awaitable[None]] | None
+    ): ...
+
+    async def async_publish(self, subject: str, payload: bytes) -> None: ...
+
+    def publish(self, subject: str, payload: bytes) -> None: ...
 
 
 class Stream(Protocol):
